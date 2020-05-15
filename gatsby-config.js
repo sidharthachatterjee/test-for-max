6f40ab59e6d01db89de2590760bbd3e750d652e9
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2020-05-15 18:17:23 +0530

    edit config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-datocms`,
       options: {
         apiToken: process.env.DATO_API_TOKEN,
       },
     },
+    `gatsby-transformer-remark`,
   ],
 }
